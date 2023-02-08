ae13476f02eb9465d1b4a2aea1a4c4fbdc077417
Seu nome <luizhds12@hotmail.com>
2023-02-08 16:21:49 -0300

    Maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { getVenue, showVenue } from "@mappedin/mappedin-js";
+
+const options = {
+  venue: "mappedin-demo-mall",
+  clientId: "5eab30aa91b055001a68e996",
+  clientSecret: "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1"
+};
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  @ViewChild("app", { static: false }) mapEl!: ElementRef<HTMLElement>;
+
+  async ngOnInit(): Promise<void> {
+    const venue = await getVenue(options);
+    const mapView = await showVenue(this.mapEl.nativeElement, venue);
+  }
 }
